b1a411b477dd66445c48e35acbe5a2e978e2dd64
daniele zurico <daniele.zurico@capgemini.com>
2019-11-06 10:52:28 +0000

    add a minimum styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,11 +20,7 @@ const IndexPage = ({
       <SEO title="Home" />
       <h1>Hi people</h1>
       <p>Welcome to your new Gatsby site with github actions second deploy.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
-      <Link to="/page-2/">Go to page 2</Link>
+      {/* <Link to="/page-2/">Go to page 2</Link> */}
       <div className={styles.gridContainer}>{Posts}</div>
     </Layout>
   )
