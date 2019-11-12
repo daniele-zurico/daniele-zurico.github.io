49d445a1927be0c16584c03df7a549883dac90f6
daniele zurico <daniele.zurico@capgemini.com>
2019-11-12 12:03:54 +0000

    try to make an animation on the header

--- src/components/header.js
+++ src/components/header.js
@@ -5,10 +5,10 @@ import { ThemeToggler } from "gatsby-plugin-dark-mode"
 import styles from "./header.module.css"
 import FeaturedImage from "./featuredImage"
 import ToggleDarkLight from "./toggleDarkLight"
+
 const Header = ({ title, featuredImage = true }) => {
   React.useEffect(() => {
     const theme = localStorage.getItem("theme")
-    console.log("theme", theme)
     if (!theme) {
       new Date().getTime() > 14
         ? localStorage.setItem("theme", "night")
@@ -16,20 +16,41 @@ const Header = ({ title, featuredImage = true }) => {
     }
   }, [])
 
+  let currentRef = React.useRef(null)
+
+  const animateActiveLink = evt => {
+    currentRef.current = evt.currentTarget
+  }
   return (
     <header className={!featuredImage ? styles.noFeaturedImage : null}>
       <div className={styles.fixedHeader}>
-        <div>
-          <Link to="/" className={styles.link}>
-            Blog
+        <div className={styles.linkContainer}>
+          <div
+            className={[styles.slider].join(" ")}
+            style={{
+              width: currentRef.current ? currentRef.current.offsetWidth : 38,
+              left: currentRef.current ? currentRef.current.offsetLeft : 40,
+            }}
+          />
+          <Link to="/" className={styles.link} onClick={animateActiveLink}>
+            <span>Blog</span>
           </Link>
-          <Link to="/aboutMe" className={styles.link}>
-            About Me
+          <Link
+            to="aboutMe"
+            className={styles.link}
+            onClick={animateActiveLink}
+          >
+            <span>About Me</span>
           </Link>
-          <Link to="/contacts" className={styles.link}>
-            Contacts
+          <Link
+            to="contacts"
+            className={styles.link}
+            onClick={animateActiveLink}
+          >
+            <span>Contacts</span>
           </Link>
         </div>
+
         <div style={{ marginRight: "48px" }}>
           <ThemeToggler theme="light">
             {({ theme, toggleTheme }) => {
