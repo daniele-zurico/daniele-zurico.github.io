c39c0165417b055e20f89d8944a869594800b61f
daniele zurico <daniele.zurico@capgemini.com>
2019-11-15 14:54:17 +0000

    fix z index

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,27 +9,29 @@ import React from "react"
 import Header from "./header"
 import "typeface-nunito-sans"
 import "./layout.css"
-const Layout = ({ children, uri }) => (
-  <>
-    <Header location={uri} />
-    <main>{children}</main>
-    <footer>
-      <svg
-        className="footer-svg"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 100 25"
-        preserveAspectRatio="none"
-      >
-        <path
-          d="M0,25 L0,12 L12,0 L75,21 L90,18 L100,24 L100,25 Z"
-          fill="#000000"
-        ></path>
-      </svg>
-      <div className="copyright">
-        <div>© {new Date().getFullYear()}, Built by Daniele Zurico</div>
-      </div>
-    </footer>
-  </>
-)
+const Layout = ({ children, uri }) => {
+  return (
+    <>
+      <Header location={uri} />
+      <main>{children}</main>
+      <footer>
+        <svg
+          className="footer-svg"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 25"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M0,25 L0,12 L12,0 L75,21 L90,18 L100,24 L100,25 Z"
+            fill="#000000"
+          ></path>
+        </svg>
+        <div className="copyright">
+          <div>© {new Date().getFullYear()}, Built by Daniele Zurico</div>
+        </div>
+      </footer>
+    </>
+  )
+}
 
 export default Layout
