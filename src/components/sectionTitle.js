d8d53d788ed019e6ce9bfa79913902ff7a6aeb07
daniele zurico <daniele.zurico@capgemini.com>
2019-11-15 17:14:53 +0000

    fix header color on scroll

--- src/components/sectionTitle.js
+++ src/components/sectionTitle.js
@@ -1,8 +1,8 @@
 import React from "react"
 import styles from "./sectionTitle.module.css"
 const SectionTitle = ({ title }) => (
-  <div className={styles.container}>
-    <span className={styles.title}>{title}</span>
+  <div className={styles.sectionContainer}>
+    <span className={styles.sectionTitle}>{title}</span>
   </div>
 )
 
