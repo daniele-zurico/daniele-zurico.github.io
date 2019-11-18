72dda04d8a8771fcdfbe2481484377294869b005
daniele zurico <daniele.zurico@capgemini.com>
2019-11-18 13:21:13 +0000

    fix header

--- src/components/sectionTitle.js
+++ src/components/sectionTitle.js
@@ -2,7 +2,7 @@ import React from "react"
 import styles from "./sectionTitle.module.css"
 const SectionTitle = ({ title }) => (
   <div className={styles.container}>
-    <span className={styles.title}>{title}</span>
+    <span>{title}</span>
   </div>
 )
 
